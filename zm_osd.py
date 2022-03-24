5ef3892a8a652f48e63d30fbfc363a31a6abc8d8
Bongaon DCC <bongaon.dcc@gmail.com>
2022-03-24 17:55:52 +0530

    create_ds() function create_ds
    1. Our objective is to create a python script for preparing zm formats
    irrespective of any division.
    2. That means it will create report of any division code if its value
    can be found in ccc.txt file and master.csv file
    3. in create_ds() function, it creates a dictionary of ccc wise class
    wise OSD (curently initalised with zero osd) and it takes ccc code from
    ccc.txt file

---
+++ zm_osd.py
@@ -0,0 +1,21 @@
+import pprint
+
+def create_ds(ccc_file):
+    non_govt_osd, govt_osd= {'live': {}, 'dd': {}}, {'live': {}, 'dd': {}}
+    with open(ccc_file, 'r') as f:
+        for line in f:
+            line= line.strip()
+            non_govt_osd['live'][line]= {'D': [0,0], 'C': [0,0], 'I': [0,0], 'A': [0,0], 'O': [0,0]}
+            non_govt_osd['dd'][line]= {'D': [0,0], 'C': [0,0], 'I': [0,0], 'A': [0,0], 'O': [0,0]}
+
+            govt_osd['live'][line]= {'dtw': [0,0], 'stw': [0,0], 'phe': [0,0], 'str': [0,0], 'muni': [0,0], 'oth': [0,0]}
+            govt_osd['dd'][line]= {'dtw': [0,0], 'stw': [0,0], 'phe': [0,0], 'str': [0,0], 'muni': [0,0], 'oth': [0,0]}
+
+    return non_govt_osd, govt_osd
+
+def main():
+    ccc_file= 'ccc.txt'
+    non_govt_osd, govt_osd= create_ds(ccc_file)
+    
+if __name__== '__main__':
+    main()
