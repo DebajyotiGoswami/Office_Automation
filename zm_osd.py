20649d902822988d149ee26c4a8ee877f7a49ccb
Bongaon DCC <bongaon.dcc@gmail.com>
2022-03-25 15:36:14 +0530

    OSD DS modified. SQL QUERY modified
    Now, our target changed to prepare such a query where column name
    will be same as dictionary key name of govt_osd and non_govt_osd
    
    our main obstacles in sql query preparation was to break up between
    municipality street light and non street light as well as non govt
    agri with govt agri and dtw.
    
    However, case function used and checked the query running correctly
    
    Query attributes' name and dictionary key names of both osd dictionary
    is same now. IT WILL HELP US TO POPULATE THE DICTIONARY EASILY.

--- zm_osd.py
+++ zm_osd.py
@@ -1,22 +1,24 @@
-import pprint
+import pprint, csv
 
 def create_ds(ccc_file):
-    non_govt_osd, govt_osd= {'live': {}, 'dd': {}}, {'live': {}, 'dd': {}}
+    non_govt_osd, govt_osd= {'LIVE': {}, 'DD': {}}, {'LIVE': {}, 'D': {}}
     with open(ccc_file, 'r') as f:
         for line in f:
             line= line.strip()
-            non_govt_osd['live'][line]= {'D': [0,0], 'C': [0,0], 'I': [0,0], 'A': [0,0], 'O': [0,0]}
-            non_govt_osd['dd'][line]= {'D': [0,0], 'C': [0,0], 'I': [0,0], 'A': [0,0], 'O': [0,0]}
+            non_govt_osd['LIVE'][line]= {'D': [0,0], 'C': [0,0], 'I': [0,0], 'A': [0,0], 'O': [0,0]}
+            non_govt_osd['DD'][line]= {'D': [0,0], 'C': [0,0], 'I': [0,0], 'A': [0,0], 'O': [0,0]}
 
-            govt_osd['live'][line]= {'dtw': [0,0], 'stw': [0,0], 'phe': [0,0], 'str': [0,0], 'muni': [0,0], 'oth': [0,0]}
-            govt_osd['dd'][line]= {'dtw': [0,0], 'stw': [0,0], 'phe': [0,0], 'str': [0,0], 'muni': [0,0], 'oth': [0,0]}
+            govt_osd['LIVE'][line]= {'DTW': [0,0], 'STW': [0,0], 'PHE': [0,0], 'STR': [0,0], 'MUNI': [0,0], 'OTH': [0,0]}
+            govt_osd['DD'][line]= {'DTW': [0,0], 'STW': [0,0], 'PHE': [0,0], 'STR': [0,0], 'MUNI': [0,0], 'OTH': [0,0]}
 
     return non_govt_osd, govt_osd
 
 def main():
     ccc_file= 'ccc.txt'
+    master_file= 'master.csv'
     non_govt_osd, govt_osd= create_ds(ccc_file)
-    pprint.pprint(govt_osd)
+    non_govt_osd, govt_osd= calculate_osd(non_govt_osd, govt_osd, master_file)
+    #pprint.pprint(govt_osd)
     
 if __name__== '__main__':
     main()
