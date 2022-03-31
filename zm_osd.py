992fd8e6cfd6d8251a16934829a8438bda1db670
Bongaon DCC <bongaon.dcc@gmail.com>
2022-03-31 15:35:47 +0530

    zm_osd_new_ds.py changed to zm_osd.py

--- zm_osd.py
+++ zm_osd.py
@@ -9,11 +9,11 @@ def create_ds(ccc_file):
     with open(ccc_file, 'r') as f:
         for line in f:
             line= line.strip()
-            non_govt_osd['LIVE'][line]= {'D': [0,0], 'C': [0,0], 'I': [0,0], 'A': [0,0], 'O': [0,0]}
-            non_govt_osd['DD'][line]= {'D': [0,0], 'C': [0,0], 'I': [0,0], 'A': [0,0], 'O': [0,0]}
+            non_govt_osd['LIVE'][line]= {'D_count': 0, 'D_osd': 0, 'C_count': 0, 'C_osd': 0, 'I_count': 0, 'I_osd': 0, 'A_count': 0, 'A_osd': 0, 'O_count': 0, 'O_osd': 0}
+            non_govt_osd['DD'][line]= {'D_count': 0, 'D_osd': 0, 'C_count': 0, 'C_osd': 0, 'I_count': 0, 'I_osd': 0, 'A_count': 0, 'A_osd': 0, 'O_count': 0, 'O_osd': 0}
 
-            govt_osd['LIVE'][line]= {'DTW': [0,0], 'STW': [0,0], 'PHE': [0,0], 'STR': [0,0], 'MUNI': [0,0], 'OTH': [0,0]}
-            govt_osd['DD'][line]= {'DTW': [0,0], 'STW': [0,0], 'PHE': [0,0], 'STR': [0,0], 'MUNI': [0,0], 'OTH': [0,0]}
+            govt_osd['LIVE'][line]= {'DTW_count': 0, 'DTW_osd': 0, 'STW_count': 0, 'STW_osd': 0, 'PHE_count': 0, 'PHE_osd': 0, 'STR_count': 0, 'STR_osd': 0, 'MUNI_count': 0, 'MUNI_osd': 0, 'OTH_count': 0, 'OTH_osd':0}
+            govt_osd['DD'][line]= {'DTW_count': 0, 'DTW_osd': 0, 'STW_count': 0, 'STW_osd': 0, 'PHE_count': 0, 'PHE_osd': 0, 'STR_count': 0, 'STR_osd': 0, 'MUNI_count': 0, 'MUNI_osd': 0, 'OTH_count': 0, 'OTH_osd':0}
 
     return non_govt_osd, govt_osd
 
@@ -24,27 +24,32 @@ def calculate_osd(master_file):
         for item in masterDict:
             if item['OSD_REMARK'].strip()== 'OSD' and item['CONN_STAT'].strip() in ('LIVE', 'DD'):
                 if item['GOVT_STAT']== 'NO':
-                    non_govt_osd[item['CONN_STAT'].strip()][item['CCC_CODE']][item['TYPE'].strip()][0]+= int(item['COUNT'])
-                    non_govt_osd[item['CONN_STAT'].strip()][item['CCC_CODE']][item['TYPE'].strip()][1]+= round(float(item['OSD'])/100000,2)
+                    non_govt_osd[item['CONN_STAT'].strip()][item['CCC_CODE']][item['TYPE'].strip()+'_count']+= int(item['COUNT'])
+                    non_govt_osd[item['CONN_STAT'].strip()][item['CCC_CODE']][item['TYPE'].strip()+'_osd']+= round(float(item['OSD'])/100000,5)
                 elif item['GOVT_STAT']== 'YES':
-                    govt_osd[item['CONN_STAT'].strip()][item['CCC_CODE']][item['TYPE'].strip()][0]+= int(item['COUNT'])
-                    govt_osd[item['CONN_STAT'].strip()][item['CCC_CODE']][item['TYPE'].strip()][1]+= round(float(item['OSD'])/100000,2)
+                    govt_osd[item['CONN_STAT'].strip()][item['CCC_CODE']][item['TYPE'].strip()+'_count']+= int(item['COUNT'])
+                    govt_osd[item['CONN_STAT'].strip()][item['CCC_CODE']][item['TYPE'].strip()+'_osd']+= round(float(item['OSD'])/100000,5)
                     
     return non_govt_osd, govt_osd
 
 def write_osd(non_govt_osd, govt_osd):
-    #fileObj= xl.Workbook()
-    #sheet= fileObj.active
-    df= pd.DataFrame.from_dict(non_govt_osd['LIVE'], orient= 'index')
-    #df= (df.T)
-    print(df)
-    df.to_excel(str(date.today())+'-ZM-OSD.xlsx')
-    #fileObj.save(str(date.today())+'-ZM-OSD.xlsx')
-    
+    writer= pd.ExcelWriter(str(date.today())+'-ZM-OSD.xlsx')
+    with writer:
+        df= pd.DataFrame.from_dict(non_govt_osd['LIVE'], orient= 'index')
+        df.to_excel(writer, sheet_name= 'live_osd', startrow= 1)
+
+        df= pd.DataFrame.from_dict(govt_osd['LIVE'], orient= 'index')
+        df.to_excel(writer, sheet_name= 'live_osd', startrow= 15)
+
+        df= pd.DataFrame.from_dict(non_govt_osd['DD'], orient= 'index')
+        df.to_excel(writer, sheet_name= 'DD_osd', startrow= 1)
+
+        df= pd.DataFrame.from_dict(govt_osd['DD'], orient= 'index')
+        df.to_excel(writer, sheet_name= 'DD_osd', startrow= 15)
+        
 def main():
     non_govt_osd, govt_osd= calculate_osd(MASTER_FILE)
     write_osd(non_govt_osd, govt_osd)
-    pprint.pprint(govt_osd)
     
 if __name__== '__main__':
     main()
